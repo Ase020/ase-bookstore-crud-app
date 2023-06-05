e08a3699a8e7279658c40af499d33b09f0b65e10
felixNyalenda <felixasejunior@gmail.com>
2023-06-05 18:15:24 +0300

    hero section build

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,7 +1,14 @@
 import "./home.scss";
 
 const Home = () => {
-  return <div className="home">Home</div>;
+  return (
+    <div className="home">
+      <h1 className="title">
+        Welcome to aseBookstore. <br /> Find your read of the day from our rich
+        catalog.
+      </h1>
+    </div>
+  );
 };
 
 export default Home;
