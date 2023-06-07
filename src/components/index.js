4b9b0ae029595061acab6425f6671878555c53b3
felixNyalenda <felixasejunior@gmail.com>
2023-06-07 13:02:36 +0300

    loading path error fix

--- src/components/index.js
+++ src/components/index.js
@@ -2,6 +2,6 @@ import { default as Navbar } from "./navbar/Navbar";
 import { default as Footer } from "./footer/Footer";
 import { default as BookLayout } from "./bookLayout/BookLayout";
 import { default as Reviews } from "./reviews/Reviews";
-import { default as Loading } from "./loading/loading";
+import { default as Loading } from "./loading/Loading";
 
 export { Navbar, Footer, BookLayout, Reviews, Loading };
